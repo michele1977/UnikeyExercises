34617dd0f3ef09efdaf86cff273082eac270fd54
andrea-77 <59687509+andrea-77@users.noreply.github.com>
2020-01-30 17:51:50 +0100

    no message

--- Ninject+Nlog+EF+Automapper/Ninject+Nlog+EF+Automapper/Exercise.Business/MyService.cs
+++ Ninject+Nlog+EF+Automapper/Ninject+Nlog+EF+Automapper/Exercise.Business/MyService.cs
@@ -25,5 +25,10 @@ namespace Exercise.Business
         {
             Repository.Delete(id);
         }
+
+        public List<Assesment> GetTestList(string text)
+        {
+            return Repository.GetTestList(text);
+        }
     }
 }
